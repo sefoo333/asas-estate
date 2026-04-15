0696738d0e527353e2192cd75235357ccdf6b8d4
sefoo333 <seifali101z@gmail.com>
2026-04-15 06:00:18 +0200

    update files

--- src/app/api/brokers/createBroker/route.ts
+++ src/app/api/brokers/createBroker/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 
 
-export async function POST(req:Request, { params }: { params: { id: string }}){
+export async function POST(req:Request){
 const {userName, bio, image, company,idUser, phoneCode, phone, location, locationCode, languages} = await req.json();
 
 if (!idUser){
