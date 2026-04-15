48bbc829c5f0b09b796abddc1e967e5b1126f235
sefoo333 <seifali101z@gmail.com>
2026-04-15 11:20:10 +0200

    update files

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,27 +1,27 @@
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { neonConfig, Pool } from '@neondatabase/serverless'
-import ws from 'ws'
-import { PrismaClient } from '@/generated/prisma/client'
+// import { PrismaClient } from '@prisma/client'
+// import { PrismaNeon } from '@prisma/adapter-neon'
+// import { neonConfig, Pool } from '@neondatabase/serverless'
+// import ws from 'ws'
 
-// مطلوب في Node.js environment
-neonConfig.webSocketConstructor = ws
+// // مطلوب في Node.js environment
+// neonConfig.webSocketConstructor = ws
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
 
-function createPrismaClient() {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
+// function createPrismaClient() {
+//   const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   })
 
-  const adapter = new PrismaNeon(pool)
+//   const adapter = new PrismaNeon(pool)
 
-  return new PrismaClient({ adapter })
-}
+//   return new PrismaClient({ adapter })
+// }
 
-export const prisma =
-  globalForPrisma.prisma ?? createPrismaClient()
+// export const prisma =
+//   globalForPrisma.prisma ?? createPrismaClient()
 
-if (process.env.NODE_ENV !== 'production')
-  globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production')
+//   globalForPrisma.prisma = prisma
