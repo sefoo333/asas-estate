e07fa31aa5b446687e62280863413e6fc7b0e703
sefoo333 <seifali101z@gmail.com>
2026-04-25 15:05:32 +0300

    fix some files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-"use client"
 import Categories from "@/componants/Homepage/Categories/CategoriesSection";
 import CTA from "@/componants/Homepage/CTA";
 import { CarouselDemo } from "@/componants/Homepage/HeroSection";
 import Products from "@/componants/Homepage/ProductsSection";
 import Thanks from "@/componants/Homepage/Thanks";
-import { useState } from "react";
 
 export default  function Page() {
- const [test,setTe] = useState("")
- const [tesxt,setTe2] = useState("")
- const [tesxt2,setTe32] = useState("")
- const [tesst,setT3e] = useState("")
- const [test2,setTse] = useState("")
+
 
   return (
    <main>
